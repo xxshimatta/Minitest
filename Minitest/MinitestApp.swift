fe3306e7c5470deb82e3c212875e1c0ded2bf2af
xxshimatta <jeffreyclay00@gmail.com>
2023-11-07 22:40:25 +0700

    Initial Commit

---
+++ Minitest/MinitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinitestApp.swift
+//  Minitest
+//
+//  Created by Jeffrey Clay Setiawan on 07/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MinitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
