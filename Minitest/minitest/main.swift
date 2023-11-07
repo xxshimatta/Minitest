4e17aeef9ea64b90076cb3bbc22a78ead6c4df2d
xxshimatta <jeffreyclay00@gmail.com>
2023-11-07 23:55:36 +0700

    add ons

--- Minitest/minitest/main.swift
+++ Minitest/minitest/main.swift
@@ -7,5 +7,33 @@
 
 import Foundation
 
-print("Hello, World!")
-
+func check(num: Int) -> Bool{
+    if num == 1 || num == 0{
+        return false
+    }
+    for j in 2..<num{
+        if num%j == 0{
+            return false
+        }
+    }
+    return true
+}
+var arr : [Any] = []
+var i = 99
+while i>0{
+    if check(num: i){
+        i -= 1
+    }else{
+        if i%15 == 0{
+            arr.append("FooBar")
+        }else if i%3 == 0{
+            arr.append("Foo")
+        }else if i%5 == 0{
+            arr.append("Bar")
+        }else{
+            arr.append(i)
+        }
+        i -= 1
+    }
+}
+print(arr)
